1f5e14967c911ec2f40d135c201fdba3cc7e5c22
nabeel-khanjee <59483644+nabeel-khanjee@users.noreply.github.com>
2021-11-28 21:28:53 +0500

    Add files via upload

---
+++ RealStateController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Zabuka.Controllers
+{
+    public class RealStateController : Controller
+    {
+        // GET: RealState
+        public ActionResult RealStateDetails()
+        {
+            return View();
+        }
+
+        public ActionResult SellHome()
+        {
+            return View();
+        }
+
+        public ActionResult RentHome()
+        {
+            return View();
+        }
+
+        public ActionResult BuyHome()
+        {
+            return View();
+        }
+
+        public ActionResult HouseDetail()
+        {
+            return View();
+        }
+
+
+
+
+    }
+}
