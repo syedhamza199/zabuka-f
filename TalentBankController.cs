9fd3b542fe8d265acaeada33c7f904a21914eaed
Syed Hamza <56481553+syedhamza199@users.noreply.github.com>
2021-11-28 20:58:11 +0500

    Add files via upload

---
+++ TalentBankController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Zabuka.Controllers
+{
+    public class TalentBankController : Controller
+    {
+        //
+        // GET: /TalentBank/
+        public ActionResult TalentBank()
+        {
+            return View();
+        }
+        public ActionResult TalkUs()
+        {
+            return View();
+        }
+        public ActionResult Profile()
+        {
+            return View();
+        }
+        public ActionResult FindEmployee()
+        {
+            return View();
+        }
+	}
+}
