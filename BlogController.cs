c278980291dafc1bfcb11534aef3af15fe9f3399
nabeel-khanjee <59483644+nabeel-khanjee@users.noreply.github.com>
2021-12-18 15:58:39 +0500

    Add files via upload

---
+++ BlogController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Zabuka.Controllers
+{
+    public class BlogController : Controller
+    {
+        // GET: Blog
+        public ActionResult BlogDetail()
+        {
+            return View();
+        }
+        public ActionResult Blogs()
+        {
+            return View();
+        }
+    }
+}
---
+++ TraingAndDevelopmentController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Zabuka.Controllers
+{
+    public class TraingAndDevelopmentController : Controller
+    {
+        // GET: TraingAndDevelopment
+        public ActionResult TraingAndDevelopmentDetails()
+        {
+            return View();
+        }
+        public ActionResult TraingDetails()
+        {
+            return View();
+        }
+        public ActionResult MoreTraings()
+        {
+            return View();
+        }
+
+
+    }
+}
