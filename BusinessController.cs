d179e0b519db04b6303a45e97244404c5d856777
Syed Hamza <56481553+syedhamza199@users.noreply.github.com>
2021-12-23 14:50:15 +0500

    BusinessControlle added

---
+++ BusinessController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Zabuka.Controllers
+{
+    public class BusinessController : Controller
+    {
+        //
+        // GET: /Business/
+        public ActionResult Business()
+        {
+            return View();
+        }
+        public ActionResult RegisterBusiness()
+        {
+            return View();
+        }
+	}
+}
