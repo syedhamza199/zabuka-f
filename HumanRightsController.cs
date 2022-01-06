a88047394f5f280802e811c610dab0a9372ea222
Syed Hamza <56481553+syedhamza199@users.noreply.github.com>
2022-01-06 21:40:46 +0500

    Add files via upload

---
+++ HumanRightsController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Zabuka.Controllers
+{
+    public class HumanRightsController : Controller
+    {
+        //
+        // GET: /HumanRights/
+        public ActionResult HumanRights()
+        {
+            return View();
+        }
+        public ActionResult DonateNow()
+        {
+            return View();
+        }
+	}
+}
