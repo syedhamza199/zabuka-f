57d1d0ed0e91e658b4b45c7f5d3d492df4512978
Syed Hamza <56481553+syedhamza199@users.noreply.github.com>
2022-01-19 21:57:24 +0500

    Add files via upload

---
+++ SportsEntertainmentController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Zabuka.Controllers
+{
+    public class SportsEntertainmentController : Controller
+    {
+        //
+        // GET: /SportsEntertainment/
+        public ActionResult SportsEntertainment()
+        {
+            return View();
+        }
+        public ActionResult ContestDetails()
+        {
+            return View();
+        }
+        public ActionResult Results()
+        {
+            return View();
+        }
+	}
+}
