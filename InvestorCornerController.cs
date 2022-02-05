38cb2991aa3e9faa56667497ffad1a44f69fe990
@Hamza10 <54814111+hamzaanis10@users.noreply.github.com>
2022-02-05 20:21:39 +0500

    Add files via upload

---
+++ InvestorCornerController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Zabuka.Controllers
+{
+    public class InvestorCornerController : Controller
+    {
+        //
+        // GET: /InvestorCorner/
+        public ActionResult Home()
+        {
+            return View();
+        }
+
+        public ActionResult InvestorProfile()
+        {
+            return View();
+        }
+	}
+
+    
+    
+}
