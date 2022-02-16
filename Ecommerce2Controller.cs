d142a2a483da2530a8f0e0bae11d6050ff3232b4
@Hamza10 <54814111+hamzaanis10@users.noreply.github.com>
2022-02-17 00:11:05 +0500

    Add files via upload

---
+++ Ecommerce2Controller.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Zabuka.Controllers
+{
+    public class Ecommerce2Controller : Controller
+    {
+        //
+        // GET: /Ecommerce2/
+        public ActionResult ProductDetail()
+        {
+            return View();
+        }
+	}
+}
